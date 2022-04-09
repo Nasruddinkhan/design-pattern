cee8947b9454de54442b43c896192cc340c49543
Nasruddinkhan <nasruddinkhan44@gmail.com>
2022-04-09 21:47:57 +0530

    add the java 8 code

---
+++ src/com/mypractice/java8/appd/InnerClass.java
@@ -0,0 +1,12 @@
+package com.mypractice.java8.appd;
+
+import java.util.function.Function;
+
+public class InnerClass {
+    Function<Object, String> f = new Function<Object, String>() {
+        @Override
+        public String apply(Object obj) {
+            return obj.toString();
+        }
+    };
+}
---
+++ src/com/mypractice/java8/chap2/MeaningOfThis.java
@@ -0,0 +1,23 @@
+package com.mypractice.java8.chap2;
+
+public class MeaningOfThis
+{
+	public final int value = 4;
+	public void doIt()
+	{
+		int value = 6;
+		Runnable r = new Runnable(){
+			public final int value = 5;
+			public void run(){
+				int value = 10;
+				System.out.println(value);
+			}
+		};
+		r.run(); 
+	}
+	public static void main(String...args)
+	{       
+		MeaningOfThis m = new MeaningOfThis();
+		m.doIt(); // ???   
+	}
+}
